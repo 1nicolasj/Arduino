5c8b3e189151b5669fdc381607185fee50554df4
Nicolas <1nicolas.dasilvaa@gmail.com>
2025-07-03 22:20:35 -0300

    ADD: sensor ultrassonico e servo motor

---
+++ servoComUltraSom1.ino
@@ -0,0 +1,30 @@
+#include <HCSR04.h>
+#include <Servo.h>
+#define TRIGGER 9
+#define ECHO 8
+#define SERVO 7
+UltraSonicDistanceSensor distanceSensor(TRIGGER,ECHO);
+Servo s;
+int pos = 90;
+
+void setup(){
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(pos);
+}
+
+void loop(){
+  int distancia = 0;
+  distancia = distanceSensor.measureDistanceCm();
+  Serial.println(distancia);
+  
+  if(distancia > pos){
+    pos +=1;
+    s.write(pos);
+  }else if(distancia < pos){
+    pos -=1;
+    s.write(pos);
+  }  
+  delay(10);
+}
+
